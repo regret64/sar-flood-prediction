83a5ca0a2b064a50158328d031a7630f6c311fce
REGRET <146427903+regret64@users.noreply.github.com>
2026-04-18 14:20:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1012,5 +1012,8 @@ def debug_water():
     return jsonify(out)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
